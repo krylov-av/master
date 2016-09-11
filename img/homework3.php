8b756786f48a3a9ad37ebb736353bc37f63c058f
Alexey Krylov <krylov@ukr.net>
2016-09-11 18:18:09 +0300

    homework 3

---
+++ img/homework3.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<h1>Работа с git</h1>
+<img src='local-remote.png' height=400>
+</body>
+</html>
